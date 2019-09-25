8eca8e3f4e07b85ce5f05c3b0c41feac8dfddc75
Itadakimas <services@lekangouroo.me>
2019-09-26 00:00:07 +0200

    add basic form to the form screen

--- src/screens/FormScreen/FormScreen.js
+++ src/screens/FormScreen/FormScreen.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import {
-    Container
+    Container,
+    Content,
+    DatePicker,
+    Form,
+    Input,
+    Item,
+    Label,
+    Picker
 } from 'native-base';
 import styles from "./FormScreen.styles";
 
@@ -10,7 +17,36 @@ class FormScreen extends React.Component
     {
         return (
             <Container>
-
+                <Content>
+                    <Form>
+                        <Item stackedLabel>
+                            <Label>First name</Label>
+                            <Input/>
+                        </Item>
+                        <Item stackedLabel>
+                            <Label>Last name</Label>
+                            <Input/>
+                        </Item>
+                        <Item>
+                            <Picker
+                                mode="dropdown"
+                                placeholder="Select an item"
+                                selectedValue={undefined}>
+                                <Picker.Item label="Select an item" value={undefined}/>
+                                <Picker.Item label="Item 0" value="0"/>
+                                <Picker.Item label="Item 1" value="1"/>
+                                <Picker.Item label="Item 2" value="2"/>
+                            </Picker>
+                        </Item>
+                        <Item last>
+                            <DatePicker
+                                locale={"en"}
+                                animationType={"fade"}
+                                androidMode={"default"}
+                                placeHolderText="Birthdate"/>
+                        </Item>
+                    </Form>
+                </Content>
             </Container>
         );
     }
